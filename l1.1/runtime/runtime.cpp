90af61032c0a943d8a35be4e4119209bfc53d257
Peter Schmidt-Nielsen <schmidtnielsenpeter@gmail.com>
2018-09-20 04:35:48 -0700

    Adding very naive runtime.

---
+++ l1.1/runtime/runtime.cpp
@@ -0,0 +1,92 @@
+// l1.1 runtime
+
+#include <iostream>
+#include "runtime.h"
+
+extern "C" void obj_dec_ref(L11Obj* obj) {
+	obj->ref_count--;
+	if (obj->ref_count <= 0) {
+		if (obj->ref_count < 0)
+			l11_panic("Negative reference count!");
+		KindTable* kind_table = global_kind_table.at(obj->kind).get();
+		auto destructor = kind_table->destructor;
+		if (destructor != nullptr)
+			destructor(obj);
+	}
+}
+
+extern "C" void obj_inc_ref(L11Obj* obj) {
+	obj->ref_count++;
+}
+
+extern "C" L11Obj* obj_lookup(L11Obj* obj, const char* attribute_name, uint64_t attribute_name_len) {
+	KindTable* kind_table = global_kind_table.at(obj->kind).get();
+	std::string attribute(attribute_name, attribute_name_len);
+	L11Obj* result_obj = kind_table->member_table.at(attribute);
+	obj_inc_ref(result_obj);
+	return result_obj;
+}
+
+extern "C" L11Obj* obj_apply(L11Obj* obj, int arg_count, L11Obj** arguments) {
+	KindTable* kind_table = global_kind_table.at(obj->kind).get();
+	return kind_table->apply(obj, arg_count, arguments);
+}
+
+extern "C" void l11_new_kind(Kind new_kind) {
+	global_kind_table.insert(std::make_pair(
+		new_kind,
+		std::make_unique<KindTable>()
+	));
+}
+
+extern "C" void l11_kind_set_destructor(Kind kind, void (*destructor)(L11Obj* self)) {
+	KindTable* kind_table = global_kind_table.at(kind).get();
+	kind_table->destructor = destructor;
+}
+
+extern "C" void l11_kind_set_apply(Kind kind, L11Obj* (*apply)(L11Obj* self, int arg_count, L11Obj** arguments)) {
+	KindTable* kind_table = global_kind_table.at(kind).get();
+	kind_table->apply = apply;
+}
+
+extern "C" void l11_kind_set_member(Kind kind, const char* attribute_name, uint64_t attribute_name_len, L11Obj* member) {
+	KindTable* kind_table = global_kind_table.at(kind).get();
+	std::string attribute(attribute_name, attribute_name_len);
+	kind_table->member_table.insert(std::make_pair(
+		attribute,
+		member
+	));
+	obj_inc_ref(member);
+}
+
+extern "C" void l11_panic(const char* error_message) {
+	std::cerr << "Panic: " << error_message << std::endl;
+	std::abort();
+}
+
+extern "C" void debug_obj_summary(L11Obj* obj) {
+	std::cout << "Object: " << obj << " with ref=" << obj->ref_count << " kind=" << obj->kind << std::endl;
+}
+
+extern "C" void* debug_malloc(uint64_t bytes) {
+	return static_cast<void*>(new uint8_t[bytes]);
+}
+
+extern "C" void debug_free(void* ptr) {
+	delete static_cast<uint8_t*>(ptr);
+}
+
+extern "C" void debug_destructor(L11Obj* self) {
+	std::cout << "Debug destructor called on " << self << std::endl;
+	delete reinterpret_cast<uint8_t*>(self);
+}
+
+extern "C" L11Obj* debug_apply(L11Obj* self, int arg_count, L11Obj** arguments) {
+	std::cout << "Debug apply called on " << self << " with " << arg_count << " arguments." << std::endl;
+	for (int i = 0; i < arg_count; i++)
+		std::cout << "  Arg: " << arguments[i] << " ref=" << arguments[i]->ref_count << " kind=" << arguments[i]->kind << std::endl;
+	// Increment the self reference, to obey our protocol.
+	obj_inc_ref(self);
+	return self;
+}
+
---
+++ l1.1/runtime/runtime.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python
+
+import os, ctypes
+
+runtime_dir = os.path.dirname(os.path.realpath(__file__))
+dll = ctypes.CDLL(os.path.join(runtime_dir, "libruntime.so"))
+
+Kind = ctypes.c_ulong
+
+class L11Obj(ctypes.Structure):
+	_fields_ = [
+		("ref_count", ctypes.c_long),
+		("kind", Kind),
+	]
+
+L11ObjPtr = ctypes.POINTER(L11Obj)
+
+destructor_type = ctypes.CFUNCTYPE(None, L11ObjPtr)
+apply_type = ctypes.CFUNCTYPE(L11ObjPtr, L11ObjPtr, ctypes.c_int, ctypes.POINTER(L11ObjPtr))
+
+# Unfortunately I can't figure out how to get ctypes to respect the above types, so just use void* instead... :(
+destructor_type = apply_type = ctypes.c_void_p
+
+desc = [
+	("obj_dec_ref", None, [L11ObjPtr]),
+	("obj_inc_ref", None, [L11ObjPtr]),
+	("obj_lookup", L11ObjPtr, [L11ObjPtr, ctypes.c_char_p, ctypes.c_ulong]),
+	("obj_apply", L11ObjPtr, [L11ObjPtr, ctypes.c_int, ctypes.POINTER(L11ObjPtr)]),
+	("l11_new_kind", None, [Kind]),
+	("l11_kind_set_destructor", None, [Kind, destructor_type]),
+	("l11_kind_set_apply", None, [Kind, apply_type]),
+	("l11_kind_set_member", None, [Kind, ctypes.c_char_p, ctypes.c_ulong, L11ObjPtr]),
+	("l11_panic", None, [ctypes.c_char_p]),
+	("debug_obj_summary", None, [L11ObjPtr]),
+	("debug_malloc", ctypes.c_void_p, [ctypes.c_ulong]),
+	("debug_free", None, [ctypes.c_void_p]),
+	("debug_destructor", None, [L11ObjPtr]),
+	("debug_apply", None, [L11ObjPtr, ctypes.c_int, ctypes.POINTER(L11ObjPtr)]),
+]
+
+for name, restype, argtypes in desc:
+	f = globals()[name] = getattr(dll, name)
+	f.restype = restype
+	f.argtypes = argtypes
+
+if __name__ == "__main__":
+	# Make a new kind 1 with debug fields.
+	l11_new_kind(1)
+	l11_kind_set_destructor(1, debug_destructor)
+	l11_kind_set_apply(1, debug_apply)
+
+	# Create an L11Obj with one ref and our new kind.
+	_ptr = debug_malloc(8)
+	ptr = ctypes.cast(_ptr, L11ObjPtr)
+	ptr.contents.ref_count = 1
+	ptr.contents.kind = 1
+	debug_obj_summary(ptr)
+
+	# If x is our new object, then evaluate x(x, x)
+	arg_array = (L11ObjPtr * 2)()
+	arg_array[0] = ptr
+	arg_array[1] = ptr
+	returned_obj = obj_apply(ptr, 2, arg_array)
+	# To obey our reference ownership protocol we have to decrement this reference.
+	obj_dec_ref(returned_obj)
+
+	# Remove the last reference and therefore delete.
+	print "About to free."
+	debug_obj_summary(ptr)
+	obj_dec_ref(returned_obj)
+
+	l11_panic("Goodbye!")
+
